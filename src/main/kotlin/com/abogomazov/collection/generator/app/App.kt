99f701425c5693016984e0f6888f01ecbb23f0d7
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-08-11 17:05:45 +0200

    Add logging

--- src/main/kotlin/com/abogomazov/collection/generator/app/App.kt
+++ src/main/kotlin/com/abogomazov/collection/generator/app/App.kt
@@ -1,10 +1,11 @@
 package com.abogomazov.collection.generator.app
 
 import com.abogomazov.collection.generator.domain.TraitImportInfo
-import com.abogomazov.collection.generator.factories.AdapterFactories
 import com.abogomazov.collection.generator.domain.modules.CollectionGenerator
 import com.abogomazov.collection.generator.domain.modules.TokenExporter
 import com.abogomazov.collection.generator.domain.modules.TraitSourcesImporter
+import com.abogomazov.collection.generator.factories.AdapterFactories
+import com.abogomazov.collection.generator.logger
 
 
 class App private constructor(
@@ -14,6 +15,8 @@ class App private constructor(
 ) {
 
     companion object {
+        private val logger by logger()
+
         fun instantiate(config: Config.ValidatedConfig): App {
             val traits = config.traits.map { TraitImportInfo(it.name, it.variants) }
             return App(
@@ -25,11 +28,15 @@ class App private constructor(
     }
 
     fun run() {
+        logger.info { "Instantiation successful. Start importing traits." }
         val traits = importer.import()
+        logger.info { "${traits.size} traits imported with maximum of ${traits.maxBy { it.variants.size }.variants.size} variants." }
         val generatedImages = generator.generate(traits)
+        logger.info { "${generatedImages.size} images generated, start saving." }
         generatedImages.forEach {
             exporter.write(it)
         }
+        logger.info { "Done!" }
     }
 
 }
